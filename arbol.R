424c9e4d3e7fb5d76d97bb03ce2b132004eb34dc
Raúl Ulises Martín Hernández <martin.jurses@gmail.com>
2019-01-11 12:34:34 +0000

    añadido más código

--- arbol.R
+++ arbol.R
@@ -1,7 +1,10 @@
+# Vídeo https://www.youtube.com/watch?v=mgh4KdbYHv0
 # Algoritmos de clasificación supervisada
 # Árboles de decisión
 # Árboles de regresión variable dependiente cuantitativa
 # Árboles de clasificación variable dependiente cualitativa
+
+set.seed(1546)
 
 library(C50)  # para la base de datos
 library(rpart)  # para el árbol de decisión
@@ -25,7 +28,29 @@ arbolrpart <- rpart(Cancelación ~ ., method = "class", data = trainData)
 
 # Imprimiendo nuestro árbol de decisión
 print(arbolrpart)
-rpart.plot(arbolrpart, extra=4) # extra = 4: probabilidad de observaciones por clase
+rpart.plot(arbolrpart, extra=4, main="Árbol sin podar") # extra = 4: probabilidad de observaciones por clase
 
 printcp(arbolrpart)
+# plotcp(arbolrpart)  # lo ejecutaremos ahora pero primero el texto de abajo
+
+# Podando el árbol ¿Por qué podar el árbol?
 plotcp(arbolrpart)
+
+# esta función busca el mínimo error con which.min en la columna 'xerror' del 'arbolrpart$cptable'
+# con el ', "CP"' que hay después te devuelve el valor que esta al mismo nivel pero en la otra columna
+pArbolRpart <- prune(arbolrpart, cp = arbolrpart$cptable[which.min(arbolrpart$cptable[,"xerror"]), "CP"])
+# pArbolRpart <- prune(arbolrpart, cp = 0.01138211) # es equivalente pero tienes que buscar el xerror más
+# pequeño que interese en printcp(arbolrpart)
+
+# vemos como queda el arbol podado
+printcp(pArbolRpart)
+rpart.plot(pArbolRpart, extra=4, main="Árbol podado") # extra = 4: probabilidad de observaciones por clase
+
+# Predecir cancelaciones en testData
+testPredRpart <- predict(arbolrpart, newdata = testData, type = "class")
+
+table(testPredRpart, testData$Cancelación)
+# ¿Cómo se lee? vamos leyendo por columnas: testPredRpart dictaminó sí en 168 caso que eran sí y 39 casos que eran no....
+
+# Calculamos el % de aciertos
+(sum(testPredRpart == testData$Cancelación)) / length(testData$Cancelación) * 100
